185e5e95477600821eb3376b312dca29ffd9e85a
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-10-30 11:36:33 +0200

    setting up environment variables
    added nodemon for live server, so that everytime i refresh changes are saved, also started a server using exressjs. will add routes to dufferent pages and also connect to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';  // need to set type from package,json to module
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("server is ready");
+})
+
 app.listen(5000, () => {
     console.log("Server has started at http://localhost:5000");
 })
