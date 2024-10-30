2ce8ab7b64d91fb0cab0a2f30c473c098dba1e19
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-10-30 13:25:45 +0200

    connecting db
    installing mongodb and setting up

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';  // need to set type from package,json to module
+import dotenv from "dotenv"   // so that we will be able to use our mongo_uri db
+import { connectDB } from './config/db.js';
+
+dotenv.config();   // accessing mongo_uri to my terminal
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("server is ready");
-})
+app.get("/products", (req, res) => {
+    // res.send("server is ready");
+});
+
+// console.log(process.env.MONGO_URI);    // accessing mongo_uri to my terminal
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server has started at http://localhost:5000");
 })
