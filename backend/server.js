68b4a22023bc680f99142c04e6d472d92920411d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-10-31 13:25:35 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,16 @@ app.post("/api/products", async (req, res) => {
 });
 // console.log(process.env.MONGO_URI);    // accessing mongo_uri to my terminal
 
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"});
+    } catch (error) {
+        
+    }
+});
+
 app.listen(5000, () => {
     connectDB();
     console.log("Server has started at http://localhost:5000");
