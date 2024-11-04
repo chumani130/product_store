e1f7a86d5d481a3e6d49e4b7f339ce3397a7e1d0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-11-04 10:25:03 +0200

    Update product.controller.js

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -52,17 +52,20 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
     const {id} = req.params;
+
+    // Check if the ID is valid
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ success: false, message: "Invalid Product ID" });
     }
+
     try {
-        await Product.findByIdAndDelete(id);
-        if (!product) {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if (!deletedProduct) {
             return res.status(404).json({ success: false, message: "Product not found" });
         }
         res.status(200).json({success: true, message: "Product deleted"});
     } catch (error) {
         console.log("error in deleting product:", error.message)
-        res.status(404).json({success: false, message: "Product not found" });
+        res.status(500).json({success: false, message: "Server Error" });
     }
 };
